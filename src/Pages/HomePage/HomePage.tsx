4a647147922d53c0d063e4620bd4a4f7cc4dc82b
Anton Bochkarev <61823060+anbochkarev1991@users.noreply.github.com>
2022-09-12 18:31:57 +0300

    home page changed to draft (#25)

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,14 +1,3 @@
-import React from 'react';
-
-import { Link } from 'react-router-dom';
-
-import { Menu, Typography } from 'antd';
-
-import { GenerateCV } from 'Pages/GenerateCV/common-components/GenerateCv';
-
-import paths from 'config/routes.json';
-import { CANDIDATES, EMPLOYEES, ASSESSMENT } from './utils/constants';
-
 import { useStyles } from './styles';
 
 export const HomePage = () => {
@@ -16,20 +5,8 @@ export const HomePage = () => {
 
   return (
     <>
-      <GenerateCV />
       <div className={classes.container}>
-        <Typography.Title level={5}>Select candidate, employee or assessment</Typography.Title>
-        <Menu className={classes.candidatesMenu}>
-          <Menu.Item>
-            <Link to={paths.generateCVcandidateList}>{CANDIDATES}</Link>
-          </Menu.Item>
-          <Menu.Item>
-            <Link to={paths.generateCVemployeesList}>{EMPLOYEES}</Link>
-          </Menu.Item>
-          <Menu.Item>
-            <p>{ASSESSMENT}</p>
-          </Menu.Item>
-        </Menu>
+        There will be home page
       </div>
     </>
   );
