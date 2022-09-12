900f7d0ec9b0f42c5182b5fbd6b68bae7590f8e9
Shkuratov Artem <shuratov2001@mail.ru>
2022-09-12 14:53:28 +0300

    fix eslint

--- src/Pages/Login/utils/helpers/getFieldValidState.ts
+++ src/Pages/Login/utils/helpers/getFieldValidState.ts
@@ -1,5 +1,6 @@
 export type message_type = '' | 'error' | 'success' | 'warning' | 'validating';
 
+// eslint-disable-next-line
 export const getFieldValidState = (fieldValue: string, fieldErrors: any): message_type => {
   if (fieldErrors?.type && fieldValue) {
     return 'error';
