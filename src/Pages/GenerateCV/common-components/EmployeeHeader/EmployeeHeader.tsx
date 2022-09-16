5f2423f08a89fb8776ad02b6a19b49338f2478de
Anton Bochkarev <a.n.bochkarev1991@gmail.com>
2022-09-16 09:57:08 +0300

    unused import deleted

--- src/Pages/GenerateCV/common-components/EmployeeHeader/EmployeeHeader.tsx
+++ src/Pages/GenerateCV/common-components/EmployeeHeader/EmployeeHeader.tsx
@@ -4,7 +4,6 @@ import { GenerateCvHeader } from 'common-components/GenerateCVHeader';
 import { EmployeeTabs } from '../EmployeeTabs';
 import { PersonalInfoCard } from 'common-components/PersonalInfoCard';
 
-import { NullableField } from 'models/TNullableField';
 import { IPersonalData } from 'models/ICommon';
 
 interface IProps {
