d442c61518db4f4a173242471c28187c525e5bdd
Denis <dzianis.khomich@dektry.com>
2022-09-28 16:29:13 +0200

    extracts generatePdf from thunk

--- src/store/reducers/cvGeneration/thunks.ts
+++ src/store/reducers/cvGeneration/thunks.ts
@@ -1,28 +1,18 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { message } from 'antd';
 
-import { apiClient } from 'services/apiService';
-import endpoints from '../../../config/endpoint.json';
 import { loadCvTemplate } from 'store/reducers/cvGeneration/actionTypes';
-import { getTemplate } from 'actions/cvGeneration';
+import { generatePdf, getTemplate } from 'actions/cvGeneration';
 
 export const fetchCvGenerationTemplate = createAsyncThunk(loadCvTemplate, getTemplate);
 
 export const generateCv = createAsyncThunk('cvGeneration/generateCv', async (template: string) => {
-  try {
-    const { data } = await apiClient.post(`${endpoints.cvGeneration}`, { template }, { responseType: 'blob' });
+  const data = await generatePdf(template);
 
-    const blob = new Blob([data], { type: 'application/pdf' });
-    const url = URL.createObjectURL(blob);
-    const link = document.createElement('a');
-    link.download = `cv.pdf`;
-    link.href = url;
-    link.click();
-    link.remove();
-
-    return data;
-  } catch (error) {
-    console.error('[API_CLIENT_CV_GENERATION_ERROR]', error);
-    message.error(`Server error. Please contact admin`);
-  }
+  const blob = new Blob([data], { type: 'application/pdf' });
+  const url = URL.createObjectURL(blob);
+  const link = document.createElement('a');
+  link.download = `cv.pdf`;
+  link.href = url;
+  link.click();
+  link.remove();
 });
