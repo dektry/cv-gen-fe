914906625b9de9e3bf5ec038252991358273f5ad
DenisHomichDektry <73819600+DenisHomichDektry@users.noreply.github.com>
2022-09-29 15:47:49 +0200

    feat/CVGE-253-generate-pdf-and-save-file (#35)
    * pdf generation
    
    * extracts generatePdf from thunk
    
    * refactoring move action type
    
    Co-authored-by: Denis <dzianis.khomich@dektry.com>

--- src/actions/cvGeneration.ts
+++ src/actions/cvGeneration.ts
@@ -12,3 +12,14 @@ export const getTemplate = async (templateName: string) => {
     message.error(`Server error. Please contact admin`);
   }
 };
+
+export const generatePdf = async (template: string) => {
+  try {
+    const { data } = await apiClient.post(`${endpoints.cvGeneration}`, { template }, { responseType: 'blob' });
+
+    return data;
+  } catch (error) {
+    console.error('[API_CLIENT_CV_GENERATION_ERROR]', error);
+    message.error(`Server error. Please contact admin`);
+  }
+};
