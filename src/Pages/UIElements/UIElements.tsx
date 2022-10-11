aec01f63db4caa38ee73eca1a1f74f03a4aaf96b
Denis <dzianis.khomich@dektry.com>
2022-10-11 11:55:57 +0200

    adds proper styles for textfield

--- src/Pages/UIElements/UIElements.tsx
+++ src/Pages/UIElements/UIElements.tsx
@@ -1,10 +1,32 @@
-import { CustomInput } from 'common-components/CustomInput';
+import { useState } from 'react';
+import { FormControlLabel, Switch, TextField } from '@mui/material';
+
+import { useStyles } from './styles';
 
 export const UIElements = () => {
+  const classes = useStyles();
+
+  const [error, setError] = useState(false);
+
   return (
-    <div>
+    <div style={{ background: '#fff' }}>
       <h1>UI Elements</h1>
-      CustomInput: <CustomInput />
+      <div className={classes.row}>
+        <p>CustomInput: </p>
+        <TextField
+          label="Email"
+          placeholder="Enter email"
+          error={error}
+          helperText={'Incorrect email'}
+          onChange={() => setError(false)}
+        />
+        <FormControlLabel
+          control={
+            <Switch checked={error} onChange={() => setError(!error)} inputProps={{ 'aria-label': 'controlled' }} />
+          }
+          label="Error"
+        />
+      </div>
     </div>
   );
 };
--- src/common-components/CustomInput/CustomInput.tsx
+++
@@ -1,8 +0,0 @@
-import { TextField } from '@mui/material';
-import { TextFieldProps } from '@mui/material/TextField/TextField';
-
-type TCustomInput = TextFieldProps;
-
-export const CustomInput = (props: TCustomInput) => {
-  return <TextField {...props} />;
-};
