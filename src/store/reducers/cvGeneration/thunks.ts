6db51de7ebb700ac094c907f672aeb05a2634ee4
Denis <dzianis.khomich@dektry.com>
2022-10-14 13:22:57 +0200

    merges skillmatrix and aswears

--- src/store/reducers/cvGeneration/thunks.ts
+++ src/store/reducers/cvGeneration/thunks.ts
@@ -3,6 +3,8 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { generatePdf, getTemplate } from 'services/requests/cvGeneration';
 import { generateCv, getProfSkills, loadCvTemplate, loadGroupOfTemplates } from './actionTypes';
 import { TProfSkill } from 'Pages/CVGeneration';
+import { getAllTechAssessments, httpGetTechAssessment } from 'services/requests/techAssessment';
+import { getSkillMatrixByIds } from 'services/requests/skills';
 
 export const fetchCvGenerationTemplate = createAsyncThunk(loadCvTemplate, async (templateName: string) => {
   const template = await getTemplate(templateName);
@@ -34,6 +36,41 @@ export const downloadCv = createAsyncThunk(generateCv, async (template: string) 
 
 export const fetchProfSkills = createAsyncThunk(getProfSkills, async (userId: string) => {
   const profSkills: TProfSkill[] = [];
-  // const profSkills = await getProfSkills();
+
+  const allAssessments = await getAllTechAssessments(userId);
+
+  if (allAssessments && allAssessments.length && allAssessments[allAssessments.length - 1]) {
+    const {
+      id: lastAssessmentId,
+      level: { id: levelId },
+      position: { id: positionId },
+    } = allAssessments[allAssessments.length - 1];
+
+    const [lastAssessment, skillMatrix] = await Promise.all([
+      httpGetTechAssessment(lastAssessmentId),
+      getSkillMatrixByIds(positionId, levelId),
+    ]);
+    const answers = lastAssessment?.answers || [];
+
+    if (answers.length) {
+      skillMatrix.forEach((group: Record<string, unknown>) => {
+        const profSkillGroup: TProfSkill = { groupName: '', skills: [] };
+
+        profSkillGroup.groupName = group.value as string;
+        profSkillGroup.skills = (group.skills as Record<string, unknown>[]).map((skill) => {
+          const profSkill = { name: '', level: '' };
+
+          profSkill.name = skill.value as string;
+          profSkill.level = answers.find((answer: Record<string, unknown>) => answer.skill === skill.value)
+            ?.actual as string;
+
+          return profSkill;
+        });
+
+        profSkills.push(profSkillGroup);
+      });
+    }
+  }
+
   return profSkills;
 });
