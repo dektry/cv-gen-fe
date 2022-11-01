dc5b113e5e6d0436dfbce9aa386198ec50fc7cb5
Anton Bochkarev <61823060+anbochkarev1991@users.noreply.github.com>
2022-11-01 15:55:39 +0300

    level names in backend and frontend didnt match (#58)

--- src/Pages/GenerateCV/common-components/PositionSkillsModal/utils/constants/index.ts
+++ src/Pages/GenerateCV/common-components/PositionSkillsModal/utils/constants/index.ts
@@ -3,6 +3,6 @@ import { LevelTypesEnum } from 'models/IInterview';
 export const levelTypes: { [key in LevelTypesEnum]: string } = {
   [LevelTypesEnum.None]: 'None',
   [LevelTypesEnum.Basic]: 'Basic',
-  [LevelTypesEnum.Regular]: 'Regular',
+  [LevelTypesEnum.Expert]: 'Expert',
   [LevelTypesEnum.Advanced]: 'Advanced',
 };
