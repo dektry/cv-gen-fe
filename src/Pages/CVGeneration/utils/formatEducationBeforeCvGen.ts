4d03a3ce1763608883f192ab0dd3f58246a2374a
DenisHomichDektry <73819600+DenisHomichDektry@users.noreply.github.com>
2022-11-01 14:37:24 +0100

    adjusts formatter of education (#57)
    Co-authored-by: Denis <dzianis.khomich@dektry.com>

--- src/Pages/CVGeneration/utils/formatEducationBeforeCvGen.ts
+++ src/Pages/CVGeneration/utils/formatEducationBeforeCvGen.ts
@@ -1,10 +1,5 @@
 import { IEducation } from 'models/IEducation';
 
-export function formatEducationBeforeCvGen(education: IEducation[]) {
-  return education.map((el) => ({
-    university: el.university,
-    specialization: el.specialization,
-    startYear: el.startYear,
-    endYear: el.endYear,
-  }));
-}
+export const formatEducationBeforeCvGen = (education: IEducation[]): string[][] => {
+  return education.map((el) => [el.university, el.specialization, el.startYear + '-' + el.endYear]);
+};
